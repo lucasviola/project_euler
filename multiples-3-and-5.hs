2fa86ce85f538ae83596928f0a65b127097f7139
lucasviola <lucasmatzenbacher@gmail.com>
2015-06-02 17:01:14 -0300

    problem solved using list comprehension

--- multiples-3-and-5.hs
+++ multiples-3-and-5.hs
@@ -1,7 +1,7 @@
 -- Multiples of 3 and 5: Project Euler Problem
 -- Level 1
 
-multiplesSum :: -> [Integer] -> Integer
+-- multiplesSum :: -> [Integer] -> Integer
+multiplesSum = sum [x | x <- [1..1000], (mod) x 3 == 0 || (mod) x 5 == 0]
 
-
-main = print $ multiplesSum [1..1000]
+main = print $ multiplesSum
