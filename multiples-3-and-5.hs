f931eee947f52ae10d524b8ee1eaad06e2a0913f
lucasviola <lucasmatzenbacher@gmail.com>
2015-06-02 17:07:34 -0300

    generic enough

--- multiples-3-and-5.hs
+++ multiples-3-and-5.hs
@@ -1,7 +1,7 @@
 -- Multiples of 3 and 5: Project Euler Problem
 -- Level 1
 
--- multiplesSum :: -> [Integer] -> Integer
-multiplesSum xs = sum [x | x <- xs, (mod) x 3 == 0 || (mod) x 5 == 0]
+multiplesSum :: Integral t => [t] -> [t]
+multiplesSum xs = [x | x <- xs, (mod) x 3 == 0 || (mod) x 5 == 0]
 
-main = print $ multiplesSum [1..1000]
+main = print $ sum $ multiplesSum [1..1000]
