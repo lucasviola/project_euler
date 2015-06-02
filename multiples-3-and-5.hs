e72281844679109b1f756a77b25c8cda81687cbb
lucasviola <lucasmatzenbacher@gmail.com>
2015-06-02 17:10:31 -0300

    correcting the input

--- multiples-3-and-5.hs
+++ multiples-3-and-5.hs
@@ -4,4 +4,4 @@
 multiplesSum :: Integral t => [t] -> [t]
 multiplesSum xs = [x | x <- xs, (mod) x 3 == 0 || (mod) x 5 == 0]
 
-main = print $ sum $ multiplesSum [1..1000]
+main = print $ sum $ multiplesSum [1..999]
